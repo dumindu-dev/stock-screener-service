9321f20700902fcba7aab49a8636394b1f1bb8f3
Dumindu <dumindu.cse@gmail.com>
2024-04-13 14:48:31 +0530

    log active users

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const crypto = require('crypto');
+const Stats = require("../utils/Stats");
 
 exports.authenticate = async (req, res) => {
 	const currentUser = await User.find({userId:req.auth.userId});
@@ -8,5 +9,12 @@ exports.authenticate = async (req, res) => {
 		const newUser = new User({...req.auth,telegramToken:"tk"+randomToken});
 		await newUser.save();
 	}
+
+	try{
+		Stats.logActiveUsers(req.auth.userId);
+	}catch(err){
+		console.log("IGNORE: Error occured updating active users");
+	}
+
 	res.send({success:1,description:"Authentication is successful"});
 };
---
+++ src/utils/Stats.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const simpleOauth2 = require('simple-oauth2');
+const config = require("../config");
+
+const COUNT_ACTIVE_USERS_ENDPOINT = "/api/stats/countActiveUsers";
+
+exports.logActiveUsers = async function (userId) {
+	const authConfig = {
+		client: {
+		  id:config.statConsumerKey, 
+		  secret: config.statConsumerSecret,
+		},
+		auth: {
+		  tokenHost: config.statTokenURL,
+		  tokenPath: config.statTokenPath,
+		},
+	};
+	const oauth2Client = new simpleOauth2.ClientCredentials(authConfig);
+	const getToken = await oauth2Client.getToken();
+	const accessToken = getToken.token.access_token;
+
+	axios.post(config.statServiceURL+COUNT_ACTIVE_USERS_ENDPOINT, {
+		headers: {
+		  'Authorization': `Bearer ${accessToken}`
+		},
+		data:JSON.stringify({
+			"userId":userId
+		})
+	}).then(response =>{
+		console.log("Log event logged successfully.");
+	}).catch(error=>{
+		console.log("Error occured calling stat service.");
+	});
+
+}
