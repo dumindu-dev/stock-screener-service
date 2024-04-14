1cfdc2341c3b0bcef822f6c90570440dd240bbe7
Dumindu <dumindu.cse@gmail.com>
2024-04-14 22:24:34 +0530

    log errors

--- src/controllers/site.controller.js
+++ src/controllers/site.controller.js
@@ -8,8 +8,10 @@ exports.getLastPriceUpdateTime = async (req, res) => {
 
 exports.getActiveUserCount = async (req, res) => {
 	Stats.getActiveUserCount().then(response=>{
+		console.log(response);
 		res.send(response);
 	}).catch(err=>{
+		console.log(err);
 		res.send("N/A");
 	});
 };
